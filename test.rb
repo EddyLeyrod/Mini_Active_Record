af1e04d96ec93cd4b484f13c5e330d8da6b695f4
eduardo L <muse77@live.com.mx>
2016-04-11 12:40:30 -0500

    Esqueleto completo

---
+++ test.rb
@@ -0,0 +1,35 @@
+require_relative 'app'
+
+def assert(truthy)
+  raise "Tests failed" unless truthy
+end
+
+#chef = Chef.find(1)
+
+# # Este es un ejemplo de test ya que los nombres de los chefs son aleatorios, este test muy probablemente fallará
+# p chef[:first_name] 
+# assert p chef[:first_name] == 'Dennis'
+# assert chef[:last_name] == 'Adria'
+
+
+
+
+# Este es un ejemplo de como crear una prueba para el método 'create'. 
+chef = Chef.create(first_name: "Eduardo", last_name: "Fernández", email:"eduardo@gmail.com", phone:"930-028-2908 x7555",
+                          birthday:Time.now)
+
+#Si lees el método create este se compone de los métodos 'new' y 'save' por ello si 'create' funciona los otros dos también. 
+
+# Este es un ejemplo de una prueba para el método 'find'. 
+# Como en la linea pasada creamos al primer chef entonces podemos buscarlo con el id => 1
+# chef = Chef.find(1)
+
+# Estas son pruebas para el método '[ ]'
+# Como sabemos que el chef que creamos se llama 'Eduardo' y apellida 'Fernandez', podemos escribir la siguiente comparación. 
+p chef[:first_name]
+assert chef[:first_name] == 'Eduardo'
+assert chef[:last_name] == 'Fernández'
+
+p Chef.all
+
+puts "finished"
